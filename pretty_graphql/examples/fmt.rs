6485e9f59ee0d60623eb35a3da2690d7ed59a185
Pig Fang <g-plane@hotmail.com>
2024-08-18 16:14:22 +0800

    add examples

---
+++ pretty_graphql/examples/fmt.rs
@@ -0,0 +1,21 @@
+use pretty_graphql::{config::FormatOptions, format_text};
+use std::{env, error::Error, fs, io};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_path = env::args().nth(1).unwrap();
+    let input = fs::read_to_string(&file_path)?;
+    let options = match fs::read_to_string("config.toml") {
+        Ok(s) => toml::from_str(&s)?,
+        Err(error) => {
+            if error.kind() == io::ErrorKind::NotFound {
+                FormatOptions::default()
+            } else {
+                return Err(Box::new(error));
+            }
+        }
+    };
+
+    let formatted = format_text(&input, &options).unwrap();
+    print!("{formatted}");
+    Ok(())
+}
